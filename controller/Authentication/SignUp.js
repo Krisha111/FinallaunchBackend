37e6100d14da7cff5d512981a4450d30af865147
KrishaPatel <krishapatel466@gmail.com>
2025-11-19 01:18:29 -0500

    Updated upload route or fixed APIjiokrishajjjyuhhmmoohkkkllhddhllmjjmhh/.;jll/hhtkjlytypullhtytrdtrtoytyruryygpuf

--- controller/Authentication/SignUp.js
+++ controller/Authentication/SignUp.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import User from '../../model/User';
+import User from '../../model/User.js';
 import dotenv from 'dotenv';
 
 dotenv.config(); // ✅ Load env variables (like JWT_SECRET)
