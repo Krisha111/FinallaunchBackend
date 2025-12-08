bee8d0bcab1755d9bbaa77ce87962e465a7fe430
KrishaPatel <krishapatel466@gmail.com>
2025-12-08 11:28:07 -0500

    Updated upload route or fixed APIjiokrishajjjyuhhmmoohkkkllhddhllmjjmhh/.;jll/hhtkjlytypullhtytrdtrtoytyruryygptttrtrumxhduiytuirugyr.drelk

--- MiddleWare/authMiddleware.js
+++ MiddleWare/authMiddleware.js
@@ -46,33 +46,4 @@ export const protect = async (req, res, next) => {
     return res.status(401).json({ message: '❌ No token provided' });
   }
 };
-// export const protect = async (req, res, next) => {
-//   let token;
 
-//   if (
-//     req.headers.authorization &&
-//     req.headers.authorization.startsWith('Bearer ')
-//   ) {
-//     try {
-//       // ✅ Extract token from header
-//       token = req.headers.authorization.split(' ')[1];
-
-//       // ✅ Decode token using secret
-//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-//       // ✅ Attach user to request without password
-//       const user = await User.findById(decoded.id).select('-password');
-//       if (!user) {
-//         return res.status(401).json({ message: '❌ User not found' });
-//       }
-
-//       req.user = user;
-//       next();
-//     } catch (err) {
-//       console.error('❌ Invalid or expired token:', err.message);
-//       return res.status(401).json({ message: '❌ Not authorized, token invalid' });
-//     }
-//   } else {
-//     return res.status(401).json({ message: '❌ No token provided' });
-//   }
-// };
