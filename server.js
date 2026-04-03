9c327a9c9aa80347eb9516a6a062867b3c0cf373
KrishaPatel <krishapatel466@gmail.com>
2026-04-03 12:08:51 -0400

    Removed .env filemgljjmbbjkjjkg

--- server.js
+++ server.js
@@ -259,7 +259,7 @@ app.post('/api/bot-reply', async (req, res) => {
         'Authorization': `Bearer ${process.env.GROQ_API_KEY}`,
       },
       body: JSON.stringify({
-        model: 'llama3-8b-8192',
+        model: 'llama3-8b',
         max_tokens: 200,
         temperature: 0.9,
         messages: [
